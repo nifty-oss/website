dd8169781874087b403f1c3313fecd4c0b9afbf2
Samuel Vanderwaal <samuel.vanderwaal@gmail.com>
2024-03-10 15:02:44 -0800

    fix baseUrl after repo rename

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,12 +16,12 @@ const config = {
   url: "https://nifty-oss.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/nifty-docs/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "nifty-oss", // Usually your GitHub org/user name.
-  projectName: "docs", // Usually your repo name.
+  projectName: "nifty-docs", // Usually your repo name.
   trailingSlash: false,
 
   onBrokenLinks: "throw",
