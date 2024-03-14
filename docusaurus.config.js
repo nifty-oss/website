6e5f9a7b21d1b506c457424f4f4f625c17d746fc
Samuel Vanderwaal <samuel.vanderwaal@gmail.com>
2024-03-14 10:05:27 -0800

    update url and baseUrl for custom domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://nifty-oss.github.io/",
+  url: "https://nifty-oss.org/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/nifty-docs/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
